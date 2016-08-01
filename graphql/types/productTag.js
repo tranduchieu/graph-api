f1f855c5e8f5773afdee0069af1715984ad9eaeb
Hieu Tran <hieu.tranduc@gmail.com>
2016-08-01 11:14:22 +0700

    add Product type

---
+++ graphql/types/productTag.js
@@ -0,0 +1,43 @@
+import {
+  GraphQLString,
+  GraphQLObjectType,
+} from 'graphql';
+
+import {
+  globalIdField,
+} from 'graphql-relay';
+
+import RelayRegistry from '../relay/RelayRegistry';
+
+export function productTagResolver(_, { id }, { loaders }) {
+  return loaders.productTag.load(id);
+}
+
+const ProductTag = new GraphQLObjectType({
+  name: 'Product Tag',
+  description: 'Product tag type',
+  fields: () => ({
+    id: globalIdField('Tag'),
+    name: {
+      type: GraphQLString,
+      resolve(data) {
+        return data.get('name');
+      },
+    },
+    description: {
+      type: GraphQLString,
+      resolve(data) {
+        return data.get('description');
+      },
+    },
+    image: {
+      type: GraphQLString,
+      resolve(data) {
+        return data.get('image');
+      },
+    },
+  }),
+});
+
+RelayRegistry.registerResolverForType(ProductTag, productTagResolver);
+export default RelayRegistry.registerNodeType(ProductTag);
