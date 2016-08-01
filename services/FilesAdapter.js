050c5415568fd59bfae57bd44576da7b98d10017
Hieu Tran <hieu.tranduc@gmail.com>
2016-08-01 11:14:11 +0700

    add FilesAdapter

---
+++ services/FilesAdapter.js
@@ -0,0 +1,40 @@
+// Files Adapter
+//
+// Allows you to change the file storage mechanism.
+// https://www.npmjs.com/package/s3
+// https://github.com/parse-server-modules/parse-server-s3-adapter/blob/master/index.js
+// https://github.com/parse-server-modules/parse-server-fs-adapter/blob/master/index.js
+// ES6 Class https://googlechrome.github.io/samples/classes-es6/
+//
+// Adapter classes must implement the following functions:
+// * createFile(config, filename, data)
+// * getFileData(config, filename)
+// * getFileLocation(config, request, filename)
+//
+// Default is GridStoreAdapter, which requires mongo
+// and for the API server to be using the DatabaseController with Mongo
+// database adapter.
+
+export class FilesAdapter {
+  /* this method is responsible to store the file in order to be retrived later by it's file name
+   *
+   * @param filename the filename to save
+   * @param data the buffer of data from the file
+   * @param contentType the supposed contentType
+   * @discussion the contentType can be undefined if the controller was not able to determine it
+   *
+   * @return a promise that should fail if the storage didn't succeed
+   *
+   */
+  createFile(filename, data, contentType) {
+		console.log(filename);
+  }
+
+  deleteFile(filename) { }
+
+  getFileData(filename) { }
+
+  getFileLocation(config, filename) { }
+}
+
+export default FilesAdapter;
