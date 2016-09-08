04f9023c014ecc57f5304eee03cfc0ad52e00db7
Hieu Tran <hieu.tranduc@gmail.com>
2016-09-08 13:51:22 +0700

    add test user query by session

--- parse-example/Users.js
+++ parse-example/Users.js
@@ -18,3 +18,21 @@
 // .then(userObj => {
 //   console.log(userObj.get('addresses'));
 // });
+
+// Query user by sessioToken
+// -----------------------------
+// const query = new Parse.Query(Parse.User);
+// query.first({
+//   sessionToken: 'r:dc7abba1027637aa625587dffd7214fd',
+// })
+// .then(console.log)
+// .catch(console.error);
+
+// const query = new Parse.Query('_Session');
+// query.equalTo('sessionToken', 'r:dc7abba1027637aa625587dffd7214fd');
+// query.include('user');
+// query.first({ useMasterKey: true })
+// .then(session => {
+//   console.log(session.get('user'));
+// })
+// .catch(console.error);
