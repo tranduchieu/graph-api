b5c2ac693e6385fb593ab9381bfe8c291e11f3d6
Hieu Tran <hieu.tranduc@gmail.com>
2016-09-12 22:45:38 +0700

    add new args query

--- graphql/loaders/product.js
+++ graphql/loaders/product.js
@@ -23,10 +23,14 @@ export const productByIdLoader = new DataLoader(ids => {
 export const allProductsLoader = new DataLoader(keys => {
   return Promise.all(keys.map(key => {
     const args = JSON.parse(key);
-    const { after, first, code } = args;
+    const { after, first, code, shop, status, boxes } = args;
     const Product = Parse.Object.extend('Product');
     const queryProduct = new Parse.Query(Product);
     if (code) queryProduct.equalTo('code', code);
+    if (shop) queryProduct.equalTo('shop', shop);
+    if (status) queryProduct.equalTo('status', status);
+    if (boxes) queryProduct.containedIn('boxes', boxes);
+    queryProduct.descending('createdAt');
     queryProduct.skip(after ? cursorToOffset(after) + 1 : 0);
     queryProduct.limit(first || 20);
 
