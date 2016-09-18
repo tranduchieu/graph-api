f144aba38518074e36806715c32e1fe9a1b80a70
Hieu Tran <hieu.tranduc@gmail.com>
2016-09-19 00:09:36 +0700

    Address coding

--- graphql/mutations/Address.js
+++ graphql/mutations/Address.js
@@ -1,30 +1,30 @@
-import {
-  GraphQLID,
-  GraphQLInt,
-  GraphQLBoolean,
-  GraphQLList,
-  GraphQLString,
-  GraphQLNonNull,
-  GraphQLInputObjectType,
-} from 'graphql';
+// import {
+//   GraphQLID,
+//   GraphQLInt,
+//   GraphQLBoolean,
+//   GraphQLList,
+//   GraphQLString,
+//   GraphQLNonNull,
+//   GraphQLInputObjectType,
+// } from 'graphql';
 
-import {
-  fromGlobalId,
-  mutationWithClientMutationId,
-  offsetToCursor,
-} from 'graphql-relay';
+// import {
+//   fromGlobalId,
+//   mutationWithClientMutationId,
+//   offsetToCursor,
+// } from 'graphql-relay';
 
-import Parse from 'parse/node';
+// import Parse from 'parse/node';
 
-import AddressType from '../types/address';
+// import AddressType from '../types/address';
 
-import { AddressEdge } from '../connections/address';
-import ViewerQueries from '../queries/Viewer';
+// import { AddressEdge } from '../connections/address';
+// import ViewerQueries from '../queries/Viewer';
 
-const AddressCreateMutation = mutationWithClientMutationId({
-  name: 'AddressCreate',
-  inputFields: {
-    
-  },
+// const AddressCreateMutation = mutationWithClientMutationId({
+//   name: 'AddressCreate',
+//   inputFields: {
 
-})
+//   },
+
+// })
