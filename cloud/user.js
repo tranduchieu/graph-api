307795c77771496ae5113e456eb41f34c554203e
Hieu Tran <hieu.tranduc@gmail.com>
2016-09-30 22:14:54 +0700

    fix email undefined

--- cloud/user.js
+++ cloud/user.js
@@ -50,7 +50,7 @@ Parse.Cloud.beforeSave(Parse.User, async (req, res) => {
 
   const fbAuthData = (req.object.get('authData') || {}).facebook || {};
 
-  if (fbAuthData && !fbAuthData.email) fbAuthData.email = `${fbAuthData.id}@fakemail.com`;
+  if (fbAuthData.id && !fbAuthData.email) fbAuthData.email = `${fbAuthData.id}@fakemail.com`;
 
   const email = fbAuthData.email || user.get('email');
   const name = fbAuthData.name || user.get('name') || null;
